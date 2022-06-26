21777b834f2b1db1137eaf43c01094ccedb8321b
Biplo12 <robertbiplosek@gmail.com>
2022-06-27 01:31:29 +0200

    Bot working

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const axios = require("axios");
 const rwClient = require("./src/twitterClient.js");
 const cronJob = require("cron").CronJob;
 
-const instance = axios.create({
-  baseURL: "https://api.github.com/users/Biplo12/events",
-  headers: {
-    client_id: process.env.GITHUB_CLIENT_ID,
-    client_secret: process.env.GITHUB_CLIENT_SECRET,
-  },
-});
+// const instance = axios.create({
+//   baseURL: "https://api.github.com/users/Biplo12/events",
+//   headers: {
+//     client_id: process.env.GITHUB_CLIENT_ID,
+//     client_secret: process.env.GITHUB_CLIENT_SECRET,
+//   },
+// });
 
 const date = new Date();
 const currentDate = `${date.getFullYear()}-${
   date.getMonth() <= 9 ? `0${date.getMonth()}` : date.getMonth()
 }-${date.getDate()}`;
 
-instance.get("https://api.github.com/users/Biplo12/events").then((res) => {
+axios.get("https://api.github.com/users/Biplo12/events").then((res) => {
   try {
     const currentRes = res.data?.map((d) => d.created_at);
     const dailyCommitsList = currentRes.filter((n) =>
@@ -25,14 +25,19 @@ instance.get("https://api.github.com/users/Biplo12/events").then((res) => {
   } catch (err) {
     console.log(err);
   }
-});
+}),
+  {
+    headers: {
+      Authorization: `token ${process.env.GITHUB_PERSONAL_TOKEN}`,
+    },
+  };
 
 const tweet = async () => {
   try {
     await rwClient.v1.tweet(
       `On ${date.toLocaleString("en-US", {
         month: "long",
-      })} ${date.getDate()} i commited ${dailyCommitsNumber} times${
+      })} ${date.getDate()} I commited ${dailyCommitsNumber} times${
         dailyCommitsNumber === 0 ? " :(" : "!"
       }`
     );
@@ -42,7 +47,7 @@ const tweet = async () => {
 };
 
 // App runs every day at 12 o'clock
-const job = new cronJob("* * * * *", () => {
+const job = new cronJob("0 12 * * *", () => {
   tweet();
 });
 
--- src/githubFetch.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const res = axios.get("https://api.github.com/users/Biplo12/events", () => {
-  try {
-    console.log(res);
-  } catch (err) {
-    console.log(err);
-  }
-});
