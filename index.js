7cff8aff9fb54920967313e1e7177372565acdd3
Biplo12 <robertbiplosek@gmail.com>
2022-06-26 22:07:57 +0200

    make it better

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const cronJob = require("cron").CronJob;
 
 const tweet = async () => {
   try {
-    await rwClient.v1.tweet("Hello!");
+    await rwClient.v1.tweet("Tweeted");
   } catch (err) {
     console.log(err);
   }
 };
 
 // App runs every day at 12 o'clock
-const job = new cronJob("1 * * * *", () => {
+const job = new cronJob("* * * * *", () => {
   tweet();
 });
 
