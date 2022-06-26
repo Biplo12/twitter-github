d4b6326d2889ba349847e69beda38db5d07596c4
Biplo12 <robertbiplosek@gmail.com>
2022-06-27 01:17:01 +0200

    Bot wotking!

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const instance = axios.create({
     client_secret: process.env.GITHUB_CLIENT_SECRET,
   },
 });
+
 const date = new Date();
 const currentDate = `${date.getFullYear()}-${
   date.getMonth() <= 9 ? `0${date.getMonth()}` : date.getMonth()
@@ -17,32 +18,32 @@ const currentDate = `${date.getFullYear()}-${
 instance.get("https://api.github.com/users/Biplo12/events").then((res) => {
   try {
     const currentRes = res.data?.map((d) => d.created_at);
-    console.log(currentRes.filter((n) => n === currentDate));
+    const dailyCommitsList = currentRes.filter((n) =>
+      n.includes(currentDate) ? n : ""
+    );
+    dailyCommitsNumber = Object.keys(dailyCommitsList).length;
   } catch (err) {
     console.log(err);
   }
-  //   const commits = res.data?.map((i) =>
-  //     i.payload.commits?.map((c) => c.message)
-  //   );
-  //   console.log(commits);
 });
 
-// let counter = 1;
-// const tweet = async (message) => {
-//   try {
-//     await rwClient.v1.tweet(message);
-//   } catch (err) {
-//     if (err.code === 403) {
-//       console.log(err.code);
-//       counter += 1;
-//     }
-//   }
-// };
+const tweet = async () => {
+  try {
+    await rwClient.v1.tweet(
+      `On ${date.toLocaleString("en-US", {
+        month: "long",
+      })} ${date.getDate()} i commited ${dailyCommitsNumber} times${
+        dailyCommitsNumber === 0 ? " :(" : "!"
+      }`
+    );
+  } catch (err) {
+    console.log(err.code);
+  }
+};
 
 // App runs every day at 12 o'clock
-// const job = new cronJob("* * * * *", () => {
-//   tweet(`Tweeted ${counter}`);
-//   console.log("Tweeted successfully");
-// });
+const job = new cronJob("* * * * *", () => {
+  tweet();
+});
 
-// job.start();
+job.start();
