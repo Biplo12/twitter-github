3a382377a165f4940d61fd0b745f70bac0533364
Biplo12 <robertbiplosek@gmail.com>
2022-06-28 15:56:51 +0200

    readme updated

--- index.js
+++ index.js
@@ -2,14 +2,6 @@ const axios = require("axios");
 const rwClient = require("./src/twitterClient.js");
 const cronJob = require("cron").CronJob;
 
-// const instance = axios.create({
-//   baseURL: "https://api.github.com/users/Biplo12/events",
-//   headers: {
-//     client_id: process.env.GITHUB_CLIENT_ID,
-//     client_secret: process.env.GITHUB_CLIENT_SECRET,
-//   },
-// });
-
 const date = new Date();
 const currentDate = `${date.getFullYear()}-${
   date.getMonth() <= 9 ? `0${date.getMonth()}` : date.getMonth()
@@ -47,7 +39,7 @@ const tweet = async () => {
 };
 
 // App runs every day at 12 o'clock
-const job = new cronJob("05 12 * * *", () => {
+const job = new cronJob("00 12 * * *", () => {
   tweet();
 });
 
